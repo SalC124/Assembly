967fd994ad109e67136a56bc7d45b180b17844e9
SalC124 <saltcal124@gmail.com>
2024-12-26 14:42:15 -0500

    while loop be like:

--- ex4.asm
+++ ex4.asm
@@ -5,6 +5,9 @@ _start:
   mov ebx, 1    ; start ebx at 1
   mov ecx, 4    ; number of iterations
 label:
-  add ebx, ebx  ; ebx += edx
+  add ebx, ebx  ; ebx += ebx
   dec ecx       ; ecx -= 1
-  
+  cmp ecx, 0    ; compare ecx with 0
+  jg label      ; jump to "label" if greater
+  mov eax, 1    ; sys_exit syscall
+  int 0x80      ; interrupt for syscalls
